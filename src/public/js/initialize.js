23510563d926e95b9a968cae0a53d0b1b63356aa
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2019-04-05 19:59:46 +0200

    Implement user interface prototype

---
+++ src/public/js/initialize.js
@@ -0,0 +1,24 @@
+var language = {
+    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+    months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    today: 'Today',
+    clear: 'Clear',
+    dateFormat: 'mm/dd/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 1
+};
+
+// minimum date is yesterday
+var today = new Date();
+
+var datePicker = $('#inputDatePicker').datepicker({
+    language: language,
+    toggleSelected: false,
+    todayButton: today,
+    minDate: today
+}).data('datepicker');
+
+datePicker.selectDate(today);
