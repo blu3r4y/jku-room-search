be3642c07051c47de18deb5c114212ff7b5a1c34
Mario Kahlhofer <mario.kahlhofer@gmail.com>
2019-06-09 13:21:04 +0200

    Minimize js and css files and cleanup webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,104 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
 
-// extract a separate main.css file to avoid FOUC
-const CssPlugin = require("mini-css-extract-plugin")
+// extract a separate css file to avoid FOUC
+const CssPlugin = require("mini-css-extract-plugin");
 
 // help copying static assets
-const CopyPlugin = require('copy-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
 
 // inject some variables into the index.html
-const HtmlPlugin = require('html-webpack-plugin')
+const HtmlPlugin = require("html-webpack-plugin");
+
+// minimizer for js
+const JsMinimizerPlugin = require("terser-webpack-plugin");
+
+// minimizer for css
+const CssMinimizerPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    devtool: 'source-map',
-    entry: './src/index.ts',
+    target: "web",
+    mode: "production",
+    devtool: "source-map",
+    entry: "./src/index.ts",
     output: {
-        filename: './js/[name].js',
-        path: path.resolve(__dirname, 'dist')
+        filename: "./js/[name].js",
+        path: path.resolve(__dirname, "dist"),
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: [".tsx", ".ts", ".js"],
     },
     module: {
-        rules: [
-            {
+        rules: [{
                 // load css stylesheets, extract source maps and extract them separately
                 test: /\.css$/,
-                use: [
-                    { loader: CssPlugin.loader },
-                    'css-loader?sourceMap'
-                ]
+                use: [{
+                        loader: CssPlugin.loader,
+                    },
+                    "css-loader?sourceMap",
+                ],
             },
             {
                 // load and compile type script sources
                 test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
+                use: "ts-loader",
+                exclude: /node_modules/,
             },
             {
                 // lint typescript files
                 test: /\.ts$/,
-                enforce: 'pre',
-                use: 'tslint-loader',
-            }
-        ]
+                enforce: "pre",
+                use: "tslint-loader",
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            // minimize javascript and build source map
+            new JsMinimizerPlugin({
+                sourceMap: true,
+            }),
+            // minimize css and build (and link) source map
+            new CssMinimizerPlugin({
+                cssProcessorOptions: {
+                    map: {
+                        inline: false,
+                        annotation: true,
+                    },
+                },
+            }),
+        ],
     },
     plugins: [
         // add the global data url
-        new webpack.DefinePlugin(
-            {
-                "DATA_URL": JSON.stringify("./data/rooms.json")
-            }),
+        new webpack.DefinePlugin({
+            "DATA_URL": JSON.stringify("./data/rooms.json"),
+        }),
         // expose the build hash as an environment variable
         new webpack.ExtendedAPIPlugin(),
         // add jquery, if we observe that its being used
-        new webpack.ProvidePlugin(
-            {
-                $: "jquery",
-                jQuery: "jquery"
-            }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+        }),
         // extract css files to a separate file
-        new CssPlugin(
-            {
-                filename: "./css/[name].css"
-            }),
+        new CssPlugin({
+            filename: "./css/[name].css",
+        }),
         // copy static assets
-        new CopyPlugin([
-            {
-                from: './src/public/'
-            }]),
+        new CopyPlugin([{
+            from: "./src/public/",
+        }]),
         // inject variables into html files
-        new HtmlPlugin(
-            {
-                template: './src/index.ejs',
-                hash: true
-            })
+        new HtmlPlugin({
+            template: "./src/index.ejs",
+            hash: true,
+        }),
     ],
     performance: {
         // only warn if assets are larger than 1 MiB
         maxEntrypointSize: 1024000,
         maxAssetSize: 1024000,
-        hints: 'warning'
-    }
-}
+        hints: "warning",
+    },
+};
